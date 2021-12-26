cb8620d2183d25b6a90a6ae6e59d58cee9780217
Freddy Drennan <drennanfreddy@gmail.com>
2021-12-25 22:15:12 -0700

    save

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,50 +1,10 @@
-use extendr_api::prelude::*;
+//! TODO: Write crate docs
 
-/// Return string `"Hello world!"` to R.
-/// @export
-#[extendr]
-fn hello_world() -> &'static str {
-    "Hello world!"
-}
+// #![doc(html_root_url = "https://docs.rs/{{CRATE}}/0.0.0")]
+// Deny a few warnings in doctests, since rustdoc `allow`s many warnings by default
+#![doc(test(attr(deny(unused_imports, unused_must_use))))]
+#![warn(missing_debug_implementations, rust_2018_idioms)]
 
-/// Addition of Integers
-/// @export
-#[extendr]
-fn add_float(x: u32, y: u32) -> u32 {
-    x + y
-}
+mod readme;
+mod rustr;
 
-/// Return a vector
-/// @export
-#[extendr]
-fn return_vec(x: i32, y: i32) -> Robj {
-    let vec_val = r!([x, y]);
-    vec_val
-}
-
-/// Return robj
-/// @export
-#[extendr]
-fn return_obj(x: Robj) -> Robj {
-    x
-}
-
-/// my_sum
-/// @export
-#[extendr]
-pub fn my_sum(v: Robj) -> Robj {
-    rprintln!("{:?}", v);
-    v
-}
-
-// Macro to generate exports.
-// This ensures exported functions are registered with R.
-// See corresponding C code in `entrypoint.c`.
-extendr_module! {
-    mod rustr;
-    fn hello_world;
-    fn add_float;
-    fn return_vec;
-    fn return_obj;
-    fn my_sum;
-}
