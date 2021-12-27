4852d0fb8262fae532209a5099e9551a069636d2
Freddy Drennan <drennanfreddy@gmail.com>
2021-12-26 17:44:04 -0700

    save

--- R/extendr-wrappers.R
+++ R/extendr-wrappers.R
@@ -7,33 +7,3 @@
 #' @usage NULL
 #' @useDynLib rustr, .registration = TRUE
 NULL
-
-#' Return string `"Hello world!"` to R.
-#' @export
-hello_world <- function() .Call(wrap__hello_world)
-
-#' Addition of Integers
-#' @export
-add_float <- function(x, y) .Call(wrap__add_float, x, y)
-
-#' Return a vector
-#' @export
-return_vec <- function(x, y) .Call(wrap__return_vec, x, y)
-
-#' Return robj
-#' @export
-return_obj <- function(x) .Call(wrap__return_obj, x)
-
-#' my_sum
-#' @export
-my_sum <- function(v) .Call(wrap__my_sum, v)
-
-#' pass_string
-#' @export
-pass_string <- function(text) .Call(wrap__pass_string, text)
-
-#' execute_lr
-#' TODO execute_lf
-#' @export
-execute_lr <- function() invisible(.Call(wrap__execute_lr))
-
